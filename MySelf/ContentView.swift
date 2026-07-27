e0efef8482fe0f577c3ca2ac075fb67e551a31e1
Sólon Rios Soares <solonriossoares1@gmail.com>
2026-07-27 18:31:50 -0300

    Initial Commit

---
+++ MySelf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySelf
+//
+//  Created by Solon Rios Soares on 27/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySelf/MySelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySelfApp.swift
+//  MySelf
+//
+//  Created by Solon Rios Soares on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MySelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
