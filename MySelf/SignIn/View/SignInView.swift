0de56ad44dbd20f318c8238d55ed07ece9a7673f
Sólon Rios Soares <solonriossoares1@gmail.com>
2026-07-30 22:00:39 -0300

    Adjustment to the sign-up button

--- MySelf/SignIn/View/SignInView.swift
+++ MySelf/SignIn/View/SignInView.swift
@@ -15,32 +15,44 @@ struct SignInView: View {
     
     @State var username: String = ""
     @State var password: String = ""
-
+    
+    @State var action: Int? = 0
+    
     var body: some View {
-        NavigationView {
-            ScrollView(showsIndicators: false) {
-                VStack(alignment: .center, spacing: 8) {
-                    Image("MySelf_logo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 220, height: 220)
-
-                    Text("Login")
-                        .foregroundColor(.blue)
-                        .font(Font.system(.title).bold())
-                        .padding(8)
-
-                    usernameField
-
-                    passwordField
-
-                    signInButton
+        ZStack {
+            if case SignInUIState.goToHomeScreen = viewModel.uiState { // virifica se o UIState recebe o estado de goToHomeScreen, se receber muda a tela
+                Text("home")
+            } else {
+                NavigationView {
                     
-                    signUpButton
-                   
+                    ScrollView(showsIndicators: false) {
+                        
+                        VStack(alignment: .center, spacing: 8) {
+                            
+                            Image("MySelf_logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 220, height: 220)
+
+                            Text("Login")
+                                .foregroundColor(.blue)
+                                .font(Font.system(.title).bold())
+                                .padding(8)
+
+                            usernameField
+
+                            passwordField
+
+                            signInButton
+                            
+                            signUpLink
+                           
+                        }
+                    }
                 }
             }
         }
+        
         
     }
 }
@@ -70,20 +82,23 @@ extension SignInView {
 }
 
 extension SignInView {
-    var signUpButton: some View {
+    var signUpLink: some View {
         VStack(alignment: .center) {
             Text("Don't have an account?")
                 .foregroundColor(.gray)
                 .padding(25)
             
-            Text("Sign Up")
-                .foregroundColor(.blue)
-            
-//            NavigationLink(
-//                destination: self.viewModel.signUpView(),
-//                
-//            )
-            
+            ZStack {
+                NavigationLink(
+                    destination: SignUpView(),
+                    tag: 1,
+                    selection: $action,
+                    label: { EmptyView() })
+                    
+                Button("Sign Up") {
+                    self.action = 1
+                }
+            }
         }
     }
 }
--- MySelf/SignIn/ViewModel/SignInViewModel.swift
+++ MySelf/SignIn/ViewModel/SignInViewModel.swift
@@ -14,9 +14,8 @@ class SignInViewModel: ObservableObject {
     
     func login(username _: String, password _: String) {
         self.uiState = .loading
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { // <------- ?
-            self.uiState = .goToHomeScreen // <------- ?
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { // silacao de uma requisicao web
+            self.uiState = .goToHomeScreen // vai pra tela home
         }
     }
 }
